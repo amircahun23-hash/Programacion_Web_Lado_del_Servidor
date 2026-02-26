dc687225f9c73ea2f1f1cd1579dc49d8dfe31fe9
amircahun23-hash <amircahun23@gmail.com>
2026-02-25 20:35:20 -0500

    Add files via upload

---
+++ TemaUno_Lenguajes/Ejercicios_1.3/modulosNPM/index.js
@@ -0,0 +1,11 @@
+//Hecho por Jairo Amir Cahun Tamay
+var generateName = require('sillyname');
+import generateName from "sillyname";
+
+var sillyName = generateName();
+
+console.log(sillyName);
+
+import {randomSuperhero} from 'superheroes';
+
+console.log(randomSuperhero());
