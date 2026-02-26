b0dd510f5256f480b700d75752fe3ba48cd72e0a
amircahun23-hash <amircahun23@gmail.com>
2026-02-25 20:35:49 -0500

    Add files via upload

---
+++ TemaUno_Lenguajes/Ejercicios_1.3/reflexion.js
@@ -0,0 +1,28 @@
+//Hecho por Jairo Amir Cahun Tamay
+/* REFLEXIÓN - EJERCICIO 1.3
+
+1.  R= La diferencia principal es que un módulo nativo como 'fs' ya viene incluido 
+cuando instalamos Node.js. No necesitamos descargarlo porque forma parte 
+del sistema
+
+1. R=  el módulo nativo ya viene integrado y el de NPM se instala aparte.
+
+2.  
+R= Considera el momento en que se cargan los módulos.
+
+La diferencia está en la forma y el momento en que se cargan los módulos. 
+'Require' pertenece a CommonJS y carga los módulos en tiempo de ejecución, 
+es decir, cuando el programa llega a esa línea. En cambio, 
+
+3. 
+a)  Es importante compartir el archivo 'package.json' porque ahí se guardan 
+todas las dependencias que necesita el proyecto. Es como la receta del programa. 
+No se comparte la carpeta 'node_modules'
+
+
+b)    Si ejecutamos "npm install" en una carpeta que solo tiene el package.json, 
+NPM leerá ese archivo y descargará automáticamente todas las dependencias
+
+
+
+*/
